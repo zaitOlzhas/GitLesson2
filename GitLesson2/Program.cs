4d13e76515984501283c49a2c449921271411760
usa-olia <issakov.alexey80@gmail.com>
2023-12-11 14:46:39 +0600

    Задание выполнено добавлен код.

--- GitLesson2/Program.cs
+++ GitLesson2/Program.cs
@@ -19,7 +19,7 @@ namespace GitLesson2
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
             //ВЫполнение задания GIT
-
+            Console.WriteLine("Выполнил Исаков Алексей");
 
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
